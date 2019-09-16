86138f68381f4266f65a67a0d72779c3c1e47ab5
Beilard <gipnous@gmail.com>
2019-09-16 16:56:05 +0300

    Added object explanation txt

--- src/main/java/ua/myCompany/lesson1/utility/IFactorial.java
+++
@@ -1,5 +0,0 @@
-package ua.myCompany.lesson1.utility;
-
-public interface IFactorial {
-    int factorial (int num);
-}
--- src/main/java/ua/myCompany/lesson1/utility/IFibonacci.java
+++
@@ -1,7 +0,0 @@
-package ua.myCompany.lesson1.utility;
-
-import java.math.BigInteger;
-
-public interface IFibonacci {
-    BigInteger fibonacci(int num);
-}
