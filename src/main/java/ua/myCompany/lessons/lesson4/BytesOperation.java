2a0a756a7779b67af2c07d79de2b32daed24cbf1
Beilard <gipnous@gmail.com>
2019-09-19 15:18:41 +0300

    Remnants from the lesson 4

--- src/main/java/ua/myCompany/lessons/lesson4/BytesOperation.java
+++ src/main/java/ua/myCompany/lessons/lesson4/BytesOperation.java
@@ -5,7 +5,7 @@ public class BytesOperation {
         int a = 0;
         int b = 1;
 
-        if (a ==0 & b++ == 1) { //with && b wouldn't increase; 
+        if (a ==0 & b++ == 1) { //with && b wouldn't increase;
             System.out.println("OK");
         }
 
--- src/main/java/ua/myCompany/lessons/lesson4/Main.java
+++ src/main/java/ua/myCompany/lessons/lesson4/Main.java
@@ -2,7 +2,7 @@ package ua.myCompany.lessons.lesson4;
 
 public class Main {
     public static void main(String[] args) {
-
+  
         Integer num = null;
         int f;
         f = 5;
